69b012b4d67afcdbc7f6cf0ff73d48c4950bab0a
Javirc97 <jrc23797@gmail.com>
2023-10-12 20:49:55 +0200

    Ultima Actualización
    Añadido archivos para crear posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 // Importamos los componentes. 
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Principal from "./components/Principal/Principal";
+import PostSearch from "./components/PostSearch/PostSearch";
 
 
 
@@ -9,7 +9,7 @@ const App = () => {
     return (
     <div className='app'>
         <Header />
-        <Principal />
+        <PostSearch />
         <Footer />
     </div>
     );
--- client/src/components/PostSearch/PostSearch.jsx
+++ client/src/components/PostSearch/PostSearch.jsx
@@ -1,11 +1,24 @@
+// Importamos los hooks
+import { usePost } from '../../hooks/usePost';
+
+// Importamos los estilos
 import './PostSearch.css';
 
-const Principal = () => {
+const PostSearch = () => {
+    const {posts, loading} = usePost();
+    
     return (
     <main>
-        <h2>Bienvenidos</h2>
+        <h2>Planes y experiencias</h2>
+
+        <ul className='tweet-list'>
+        { posts?.length > 0 && 
+            posts.map((post) => {
+            return <li key={post.id}>{post.text}</li>;
+           })}
+        </ul>
     </main>
     );
 };
 
-export default Principal;
+export default PostSearch;
